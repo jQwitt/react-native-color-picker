57e705d2b5749f7ffc39de4d2a30b1028a2ed94d
Jack Witt <jwitt1452@gmail.com>
2020-10-27 20:37:33 -0700

    basic color picker

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Slider from "@react-native-community/slider";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  slider: {
+    width: 255,
+    height: 40,
+  },
+  text: {
+    backgroundColor: "black",
+    color: "white",
+    padding: 5,
   },
 });
+
+const sliderProps = {
+  style: styles.slider,
+  minimumValue: 0,
+  maximumValue: 255,
+  value: 0,
+  step: 1,
+};
+
+export default function App() {
+  const [color, setColor] = useState({ red: 0, blue: 0, green: 0 });
+
+  const getRGB = (color) =>
+    `"rgba(${color.red}, ${color.blue}, ${color.green}, 1)"`;
+
+  return (
+    <View style={[{ backgroundColor: getRGB(color) }, styles.container]}>
+      <Text style={styles.text}>
+        R: {color.red} B: {color.blue} G: {color.green}
+      </Text>
+      <Slider
+        {...sliderProps}
+        minimumTrackTintColor="red"
+        maximumTrackTintColor="red"
+        onValueChange={(value) =>
+          setColor({ red: value, blue: color.blue, green: color.green })
+        }
+      />
+      <Slider
+        {...sliderProps}
+        minimumTrackTintColor="blue"
+        maximumTrackTintColor="blue"
+        onValueChange={(value) =>
+          setColor({ red: color.red, blue: value, green: color.green })
+        }
+      />
+      <Slider
+        {...sliderProps}
+        minimumTrackTintColor="green"
+        maximumTrackTintColor="green"
+        onValueChange={(value) =>
+          setColor({ red: color.red, blue: color.blue, green: value })
+        }
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
